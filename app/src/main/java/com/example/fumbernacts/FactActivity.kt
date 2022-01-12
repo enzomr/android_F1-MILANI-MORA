01bc7c5cc6b299e5061fdcbfc92b400d72d9abc3
Enzo MORA <enmora@a106imac09.local.isima.fr>
2022-01-12 11:56:55 +0100

    adds second activity

---
+++ app/src/main/java/com/example/fumbernacts/FactActivity.kt
@@ -0,0 +1,14 @@
+package com.example.fumbernacts
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+
+class FactActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fact)
+    }
+}
--- app/src/main/java/com/example/fumbernacts/StartActivity.kt
+++ app/src/main/java/com/example/fumbernacts/StartActivity.kt
@@ -1,11 +1,26 @@
 package com.example.fumbernacts
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
 
 class StartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
+    }
+
+    fun onMathButtonClick(view: View) {
+        val intent = Intent(this, FactActivity::class.java).apply {
+        }
+        startActivity(intent)
+    }
+
+    fun onTriviaButtonClick(view: View) {
+        val intent = Intent(this, FactActivity::class.java).apply {
+        }
+        startActivity(intent)
     }
 }
