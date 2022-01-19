64405b1299e85646a22be917967a982d3f062a82
Enzo MORA <enmora@a106imac09.local.isima.fr>
2022-01-19 08:38:11 +0100

    added date picker

--- app/src/main/java/com/example/fumbernacts/StartActivity.kt
+++ app/src/main/java/com/example/fumbernacts/StartActivity.kt
@@ -5,11 +5,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
+import android.widget.DatePicker
+import java.util.*
 
 class StartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
+
+        //val datePicker = findViewById<DatePicker>(R.id.datePicker)
+        //datePicker.minDate = 0
     }
 
     fun onMathButtonClick(view: View) {
